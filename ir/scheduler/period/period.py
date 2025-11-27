1fd1a5ea4c5d2a688fd1a7046fcff4bb31703cd0
kroman-dev <roman110797@gmail.com>
2025-11-27 23:32:41 +0300

    Add period, delete enum

--- ir/scheduler/period/period.py
+++ ir/scheduler/period/period.py
@@ -1,10 +1,34 @@
 import datetime
 
+from dateutil.relativedelta import relativedelta
+
 
 class Period:
 
+    _availableTenors = ['D', 'W', 'M', 'Q', 'S', 'Y']
+
     def __init__(self, period: str):
         self._period = period
+        self._timeDelta = self._getTimeDelta()
+
+    def _getTimeDelta(self) -> relativedelta:
+        if self._period[-1] == 'D':
+            return relativedelta(days=int(self._period[:-1]))
+        elif self._period[-1] == 'W':
+            return relativedelta(weeks=int(self._period[:-1]))
+        elif self._period[-1] == 'M':
+            return relativedelta(months=int(self._period[:-1]))
+        elif self._period[-1] == 'Q':
+            return relativedelta(months=3 * int(self._period[:-1]))
+        elif self._period[-1] == 'S':
+            return relativedelta(months=6 * int(self._period[:-1]))
+        elif self._period[-1] == 'Y':
+            return relativedelta(years=int(self._period[:-1]))
+        else:
+            raise ValueError(
+                f"Incorrect period "
+                f"-> period must be like {self._availableTenors}"
+            )
 
     def advance(self, date: datetime.date) -> datetime.date:
-        pass
+        return date + self._timeDelta
