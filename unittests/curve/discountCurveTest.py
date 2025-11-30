67cb5cf21b830b92c721332a8b433c66cb1cb062
kroman-dev <roman110797@gmail.com>
2025-11-30 11:44:41 +0300

    Add test

--- unittests/curve/discountCurveTest.py
+++ unittests/curve/discountCurveTest.py
@@ -18,6 +18,18 @@ class DiscountCurveTest(TestCase):
             dayCounter=dayCounter
         )
 
+        self._sampleCurve2 = DiscountCurve(
+            dates=[date(2025, 11, 3), date(2025, 11, 5), date(2025, 11, 7)],
+            discountFactors=discountFactors,
+            dayCounter=dayCounter
+        )
+
+        self._sampleCurve3 = DiscountCurve(
+            dates=[date(2025, 11, 3), date(2026, 11, 5), date(2027, 11, 7)],
+            discountFactors=discountFactors,
+            dayCounter=dayCounter
+        )
+
     def testInterpolate(self):
         with self.subTest("behavior"):
             self.assertGreater(self._sampleCurve(date(2024, 4, 1)), 0.98)
@@ -28,3 +40,15 @@ class DiscountCurveTest(TestCase):
 
         with self.subTest("right bound"):
             self.assertEqual(0.96, self._sampleCurve(date(2025, 1, 1)))
+
+    def testCompareWithQuantlib(self):
+        with self.subTest("compare with quantlib"):
+            self.assertAlmostEqual(
+                0.9899494936611666,
+                self._sampleCurve2(date(2025, 11, 4))
+            )
+        with self.subTest("compare with quantlib long distance"):
+            self.assertAlmostEqual(
+                0.9726039674627344,
+                self._sampleCurve3(date(2027, 3, 18))
+            )
